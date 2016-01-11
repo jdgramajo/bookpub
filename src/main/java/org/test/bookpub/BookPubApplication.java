9a3884b7711b0a38c57094f96700a538eb6c5ccf
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-01-11 17:57:56 -0600

    Initial commit plus .gitignore.

---
+++ src/main/java/org/test/bookpub/BookPubApplication.java
@@ -0,0 +1,12 @@
+package org.test.bookpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPubApplication.class, args);
+	}
+}
