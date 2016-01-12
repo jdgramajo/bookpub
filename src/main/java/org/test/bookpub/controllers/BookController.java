8ba5e3e12c40c24f9c94fc7184f42d238def2fce
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-01-12 13:59:38 -0600

    Starts web REST application.

---
+++ src/main/java/org/test/bookpub/controllers/BookController.java
@@ -0,0 +1,28 @@
+package org.test.bookpub.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.test.bookpub.entity.Book;
+import org.test.bookpub.repository.BookRepository;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Iterable<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{isbn}", method = RequestMethod.GET)
+    public Book getBook(@PathVariable String isbn) {
+        return bookRepository.findBookByIsbn(isbn);
+    }
+
+}
