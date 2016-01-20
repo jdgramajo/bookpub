2bc743d0beb047a0b51b34bdebeffae3de733644
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-01-19 17:50:44 -0800

    Finishes chapter 3's excercises.
      - SSL recipe was failing, after some debugging I couldn't find the reason. Running it today simply worked.

--- src/main/java/org/test/bookpub/WebConfiguration.java
+++ src/main/java/org/test/bookpub/WebConfiguration.java
@@ -1,10 +1,16 @@
 package org.test.bookpub;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,10 +18,83 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.test.bookpub.formatters.BookFormatter;
 import org.test.bookpub.repository.BookRepository;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@PropertySource("classpath:/tomcat.https.properties")
+@EnableConfigurationProperties(WebConfiguration.TomcatSslConnectorProperties.class)
 public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @ConfigurationProperties(prefix = "custom.tomcat.https")
+    public static class TomcatSslConnectorProperties {
+        private Integer port;
+        private Boolean ssl = true;
+        private Boolean secure = true;
+        private String scheme = "https";
+        private File keystore;
+        private String keystorePassword;
+
+        public Integer getPort() {
+            return port;
+        }
+
+        public void setPort(Integer port) {
+            this.port = port;
+        }
+
+        public Boolean getSsl() {
+            return ssl;
+        }
+
+        public void setSsl(Boolean ssl) {
+            this.ssl = ssl;
+        }
+
+        public Boolean getSecure() {
+            return secure;
+        }
+
+        public void setSecure(Boolean secure) {
+            this.secure = secure;
+        }
+
+        public String getScheme() {
+            return scheme;
+        }
+
+        public void setScheme(String scheme) {
+            this.scheme = scheme;
+        }
+
+        public File getKeystore() {
+            return keystore;
+        }
+
+        public void setKeystore(File keystore) {
+            this.keystore = keystore;
+        }
+
+        public String getKeystorePassword() {
+            return keystorePassword;
+        }
+
+        public void setKeystorePassword(String keystorePassowrd) {
+            this.keystorePassword = keystorePassowrd;
+        }
+
+        public void configureConnector(Connector connector) {
+            if (port != null) connector.setPort(port);
+            if (secure != null) connector.setSecure(secure);
+            if (scheme != null) connector.setScheme(scheme);
+            if (ssl != null) connector.setProperty("SSLEnable", ssl.toString());
+            if (keystore != null && keystore.exists()) {
+                connector.setProperty("keystoreFile", keystore.getAbsolutePath());
+                connector.setProperty("keystorePassword", keystorePassword);
+            }
+        }
+
+    }
 
     @Autowired
     private BookRepository bookRepository;
@@ -43,6 +122,19 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
                 container.setSessionTimeout(1, TimeUnit.MINUTES);
             }
         };
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainerFactory(TomcatSslConnectorProperties properties) {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector(properties));
+        return tomcat;
+    }
+
+    private Connector createSslConnector(TomcatSslConnectorProperties properties) {
+        Connector connector = new Connector();
+        properties.configureConnector(connector);
+        return connector;
     }
 
 }
