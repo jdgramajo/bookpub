c355204daf949f15c5c4f610e875b35cb6f08cf5
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-02-04 16:19:19 -0600

    Adds additional data to db by loading it from a resources dir in the test classpath.
      - There was supposed to be an additional application.properties file that would allow configuration of an in memory db, however it did not work and all tests failed. Will skip that and move to Mockito!

--- src/test/java/org/test/bookpub/BookPubApplicationTests.java
+++ src/test/java/org/test/bookpub/BookPubApplicationTests.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,6 +22,8 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import org.test.bookpub.entity.Book;
 import org.test.bookpub.repository.BookRepository;
+
+import javax.sql.DataSource;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = BookPubApplication.class)
@@ -32,20 +36,34 @@ public class BookPubApplicationTests {
 	@Autowired
 	private BookRepository repository;
 
+	@Autowired
+	private DataSource ds;
+
 	@Value("${local.server.port}")
 	private int port;
 
 	private MockMvc mockMvc;
 	private RestTemplate restTemplate = new TestRestTemplate();
+	private static boolean loadDataFixtures = true;
 
 	@Before
 	public void setupMockMvc() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
 	}
 
+	@Before
+	public void loadDataFixtures() {
+		if (loadDataFixtures) {
+			ResourceDatabasePopulator populator = new ResourceDatabasePopulator(
+					context.getResource("classpath:/test-data.sql"));
+			DatabasePopulatorUtils.execute(populator, ds);
+			loadDataFixtures = false;
+		}
+	}
+
 	@Test
 	public void contextLoads() {
-		Assert.assertEquals(1, repository.count());
+		Assert.assertEquals(2, repository.count());
 	}
 
 	@Test
