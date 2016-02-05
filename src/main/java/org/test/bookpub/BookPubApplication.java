55bf521d91d075caade59ba86329debf0147e7c1
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-02-04 21:56:28 -0600

    Fixes typos and imports.

--- src/main/java/org/test/bookpub/BookPubApplication.java
+++ src/main/java/org/test/bookpub/BookPubApplication.java
@@ -3,12 +3,8 @@ package org.test.bookpub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.test.bookpubstarter.dbcount.DbCountRunner;
 import org.test.bookpubstarter.dbcount.EnableDbCounting;
-
-import java.util.Collection;
 
 @SpringBootApplication
 @EnableScheduling
